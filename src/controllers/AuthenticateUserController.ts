62f67c64b66dcab0e8f2e4c86368e84df1894166
PedroDev4 <pedrohmartinss@hotmail.com>
2021-11-24 23:06:06 -0300

    JWT Token generation and User Prisma Model added

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -5,12 +5,18 @@ import { AuthenticateUserService } from "../services/AuthenticateUserService";
 class AuthenticateUserController {
 
     async handleRequest(request: Request, response: Response): Promise<Response> {
+        const authUserService = new AuthenticateUserService();
+
         const { code } = request.body;
 
-        const authUserService = new AuthenticateUserService();
-        const result = await authUserService.authenticate(code);
+        try {
 
-        return response.json(result);
+            const result = await authUserService.authenticate(code);
+            return response.json(result);
+
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
     }
 
 }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import prismaClient from '../prisma';
+import { sign } from 'jsonwebtoken';
 
 /** 
  * Receber code(string)
@@ -15,37 +17,72 @@ interface IAccessTokenResponse {
     access_token: string;
 }
 
-interface IUserDataResponse {
+interface User {
     avatar_url: string;
     id: number;
     login: string;
     name: string;
 }
 
+interface IResponse {
+    token: string,
+    user: User
+}
+
 class AuthenticateUserService {
 
     private url: string = 'https://github.com/login/oauth/access_token';
+    private userDataUrl: string = 'https://api.github.com/user';
 
-    async authenticate(code: string): Promise<IUserDataResponse> {
+    async authenticate(code: string): Promise<IResponse> {
         const access_token = await this.getAccessToken(code);
 
-        const { data } = await axios.get<IUserDataResponse>('https://api.github.com/user', {
+        const { data } = await axios.get<User>(this.userDataUrl, {
             headers: {
                 authorization: `Bearer ${access_token}`
             }
         });
 
-        const userData: IUserDataResponse = {
-            avatar_url: data.avatar_url,
-            id: data.id,
-            login: data.login,
-            name: data.name
+        const { name, id, login, avatar_url } = data;
+
+        const user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        });
+
+        if (!user) {
+            await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
         }
 
-        return userData;
+        const token = await this.createJwtToken({
+            id,
+            avatar_url,
+            login,
+            name
+        });
+
+        const response: IResponse = {
+            token,
+            user: {
+                avatar_url,
+                id: user.github_id,
+                login,
+                name
+            }
+        };
+
+        return response;
     }
 
-    async getAccessToken(code: string): Promise<string> {
+    private async getAccessToken(code: string): Promise<string> {
         const { data: access_tokenResponse } = await axios.post<IAccessTokenResponse>(this.url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
@@ -58,6 +95,22 @@ class AuthenticateUserService {
         });
 
         return access_tokenResponse.access_token;
+    }
+
+    private async createJwtToken({ id, avatar_url, login, name }: User): Promise<string> {
+        const token = sign({
+            user: {
+                id,
+                avatar_url,
+                login,
+                name
+            }
+        }, process.env.JWT_SECRET, {
+            subject: String(id),
+            expiresIn: "1d"
+        });
+
+        return token;
     }
 
 }
